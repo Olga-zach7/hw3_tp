b7cb51b372144d6124f52466074375a3664c22b7
Зачиняева Ольга <oezachinyaeva@edu.hse.ru>
2026-06-14 14:04:49 +0000

    Add nginx config, update run.sh and README

--- run.sh
+++ run.sh
@@ -16,8 +16,11 @@ case "$1" in
   structure)         find . -not -path '*/.git/*' -not -path '*/node_modules/*' | sort ;;
   report_server)
     mkdir -p "$D"
-    docker run -d --rm --name report-server -v "$D":/usr/share/nginx/html:ro -p 8080:80 nginx:alpine
-    echo "http://localhost:8080/report.html"
+    docker run -d --rm --name report-server \
+      -v "$D":/usr/share/nginx/html:ro \
+      -v "$(pwd)/reporter/default.conf":/etc/nginx/conf.d/default.conf:ro \
+      -p 8080:80 nginx:alpine
+    echo "http://localhost:8080"
     ;;
   *) echo "build_generator | run_generator | create_local_data | build_reporter | run_reporter | structure | clear_data | inside_generator | inside_reporter | report_server" ;;
 esac
